16a3a63d557709a2b40a68bf1c284c32afa0315d
Chris Bull <christopher.bull@uoit.net>
2018-10-09 22:02:26 -0400

    Runner movement & camera
    Camera for runner is set and follows the runner movement

---
+++ GDWGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //constraints for the camera y rotation
+    private const float CAM_Y_MIN = -20.0f;
+    private const float CAM_Y_MAX = 80.0f;
+    //player speed per second (expected 60 FPS)
+    public float speed = 600.0f; //speed without time.Deltatime was 10
+
+    //camera variables for tracking camera position, target, and camera object itself
+    public Transform cameraTarget;
+    public Transform playerCam;
+    public Transform noDownWardForce;
+    public Camera NoDownWardForceCam;
+
+    //private variables to gather mouse and keyboard input
+    private Vector2 moveInput;
+    private Vector2 cameraLook;
+
+    private Vector3 ray;
+    
+    // Use this for initialization
+    void Start () {
+
+        //lock cursor to the game window, specifically the center of the window
+        Cursor.lockState = CursorLockMode.Locked;
+  
+    }
+	// Update is called once per frame
+	void Update () {
+
+        //fill moveInput with keyboard input
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        //vector containing moveInput
+        Vector3 playerControl = new Vector3(moveInput.x, 0, moveInput.y);
+        
+        //fill cameraLook with mouse input
+        cameraLook.x += Input.GetAxis("Mouse X");
+        cameraLook.y += Input.GetAxis("Mouse Y");
+
+        //clamp the camera's y rotation to prevent weird camera angles
+        cameraLook.y = Mathf.Clamp(cameraLook.y, CAM_Y_MIN, CAM_Y_MAX);
+
+        //create the rotation for camera
+        Quaternion cameraRotate = Quaternion.Euler(cameraLook.y, cameraLook.x, 0);
+        //create the rotation for the Y rotation only camera
+        Quaternion cameraRotateOnlyY = Quaternion.Euler(0, cameraLook.x, 0);
+        //update the position of the camera, using the player's position and camera rotation, alongside the distance from the camera to player
+        playerCam.position = cameraTarget.position + cameraRotate * new Vector3(1,0,-2);
+        //want the camera always looking at the player, since it's a 3rd person camera
+        playerCam.LookAt(cameraTarget);
+
+        //setup the Y rotation only camera to properly get line of sight for character movement and velocity
+        noDownWardForce.position = new Vector3(playerCam.position.x, cameraTarget.position.y, playerCam.position.z) + cameraRotateOnlyY * new Vector3(0, 0 ,0);
+        noDownWardForce.LookAt(cameraTarget);
+
+        
+        //want the player to move in the forward direction of the Y rotation onlys camera (to avoid any downward force when camera is tilted), rather than just on it's local axis
+        Vector3 moveInDirectionOfCam = NoDownWardForceCam.transform.TransformVector(playerControl);
+        //gameObject.transform.Rotate(new Vector3(0, moveInDirectionOfCam.x, 0));
+        //y isn't needed for movement
+        moveInDirectionOfCam.y = 0;
+
+        //set the velocity of the rigidbody
+        GetComponent<Rigidbody>().velocity = moveInDirectionOfCam * speed * Time.deltaTime;
+        print(GetComponent<Rigidbody>().velocity);
+
+        //create a vector outwards from behind the camera
+        Vector3 rayCastBehind = playerCam.transform.TransformDirection(Vector3.forward * -1);
+        //variable for contact point of the raycast
+        RaycastHit contact;
+
+        //send out raycasts from behind the player to check if the ray is colliding with an object behind the player
+        if (Physics.Raycast(cameraTarget.TransformPoint(new Vector3(0,0,0)), rayCastBehind, out contact, 2.0f) && contact.transform != playerCam)
+        {
+            //situate the camera at point of contact to avoid camera clipping into objects
+            playerCam.position = new Vector3(contact.point.x, contact.point.y, contact.point.z);
+        }
+        
+	}
+
+    void collision (Collision collision)
+    {
+        //GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+}
