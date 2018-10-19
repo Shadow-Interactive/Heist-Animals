657dc9be490bd63616af1e5454a1c6c5be1dfa0c
Matthew Holt <matthew.holt@uoit.net>
2018-10-19 15:22:01 -0400

    [MOD]
    MOD; F-MOD hah, goteem
    
    JK, I figured out the issue with the FMOD DLL. It loads in no problem now.

--- GDWGame/Assets/Scripts/SoundManager.cs
+++ GDWGame/Assets/Scripts/SoundManager.cs
@@ -4,46 +4,52 @@ using UnityEngine;
 
 using System.Runtime.InteropServices;
 
-public class SoundManager : MonoBehaviour {
-
-    const string dllName = "FMOD Controller";
-
-    [DllImport(dllName)]
-    public static extern void initFMOD();
-
-    [DllImport(dllName)]
-    public static extern void setPosition(float _x, float _y, float _z);
-
-    [DllImport(dllName)]
-    public static extern void setVelocity(float _x, float _y, float _z);
-
-    [DllImport(dllName)]
-    public static extern void update(float dt, int channel, bool _playing);
-
-    [DllImport(dllName)]
-    public static extern void setMode(int channel);
-
-    [DllImport(dllName)]
-    public static extern void setVolume(float _volume, int channel);
-
-    [DllImport(dllName)]
-    public static extern void createSound(string soundFile);
-
-    [DllImport(dllName)]
-    public static extern void cleanFMOD();
-
-    // Use this for initialization
-    void Start () {
-        initFMOD();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //update(Time.deltaTime, 0, true);
-	}
-
-    void OnDestroy()
+namespace SoundEngine
+{
+    public class SoundManager : MonoBehaviour
     {
-        //cleanFMOD();
+
+        const string dllName = "FMOD Controller";
+
+        [DllImport(dllName)]
+        public static extern void initFMOD();
+
+        [DllImport(dllName)]
+        public static extern void setPosition(float _x, float _y, float _z);
+
+        [DllImport(dllName)]
+        public static extern void setVelocity(float _x, float _y, float _z);
+
+        [DllImport(dllName)]
+        public static extern void update(float dt, int channel, bool _playing);
+
+        [DllImport(dllName)]
+        public static extern void setMode(int channel);
+
+        [DllImport(dllName)]
+        public static extern void setVolume(float _volume, int channel);
+
+        [DllImport(dllName)]
+        public static extern void createSound(string soundFile);
+
+        [DllImport(dllName)]
+        public static extern void cleanFMOD();
+
+        // Use this for initialization
+        void Start()
+        {
+            initFMOD();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //update(Time.deltaTime, 0, true);
+        }
+
+        void OnDestroy()
+        {
+            //cleanFMOD();
+        }
     }
 }
